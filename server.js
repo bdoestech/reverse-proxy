976ea32821bd73830501cbe4bbeb032503846ac1
Brendan Li <bdoestech@gmail.com>
2024-06-14 16:36:09 -0400

    Trying to add rate limiting step. Almost got it, had to change up the fetch operation. Still need to enable to rate limiting for JUST the fetch.

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const PORT = process.env.PORT || 8080;
-const http = require("http");
+// const http = require("http");
 const httpProxy = require("http-proxy");
+const rateLimitMiddleware = require("./middlewares/ratelimit");
+
 var express  = require('express');
 var app = express();
+// app.use(rateLimitMiddleware);
 
 apiProxy = httpProxy.createProxyServer();
+app.get('/',(req, res) => {res.sendFile(__dirname + '/index.html');});
 
-app.get("/", (req, res) => {
+app.get("/url/", async (req, res) => {
+    // const url = "http://horror-forms.bdoestech.com/movies-brendan";
     const {url} = req.query;
-    apiProxy.web(req, res, {target: url});
+
+    console.log(url);
+    await fetch(url)
+    .then(response => {
+        if (!response.ok) {throw new Error('Network response was not ok');}
+        return response.json();
+    })
+    .then(data => {
+        console.log('Data received for Darayus');
+        console.log(data);  
+    })
+    // apiProxy.web(req, res, {target: "http://horror-forms.bdoestech.com/movies-brendan"});
 });
 
 
